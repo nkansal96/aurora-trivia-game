d2059c5642236a4053d2a51564f78848cbae2f11
SethEisner <30507927+SethEisner@users.noreply.github.com>
2018-04-29 19:53:53 -0700

    Update aurora-poc.cpp

--- aurora-poc.cpp
+++ aurora-poc.cpp
@@ -9,14 +9,9 @@
 int main(){
 	
 	int pipefd[2];
-	//int write_pipefd[2];
 	pid_t cpid;
-	char buf;
-	
 	char * argv[3] = {(char*)"Command-line", (char*)".", NULL};
-	
 	pipe(pipefd);
-	//pipe(parent_pipefd);
 	cpid = fork();
 	
 	
@@ -24,17 +19,14 @@ int main(){
 	if(cpid == 0){
 		//set child side of pipe to stdin of zork to pass it commands
 		dup2(pipefd[0],0);
-		
+		//start zork
 		execv("./zork", argv);
 	}
 	else{
+		//used while true as an easy way to keep the parent process running
 		while(true){
-			
-			
-			
-			 
+			//send command 'open mailbox' to child game
 			write(pipefd[1],"open mailbox\n",13);
-			//std::cout << "hi there" <<std::endl;
 			
 			sleep(5);
 		}
