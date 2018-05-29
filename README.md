38232a615496dd428c1fcdacf050f25211f4f477
Ben Lawson <blinternetgeek@gmail.com>
2018-05-29 00:21:10 -0700

    CMake configuration & code refactoring, build instructions

---
+++ README.md
@@ -0,0 +1,9 @@
+# Aurora C++ Trivia Game
+
+This is a simple proof of concept for Aurora C++ API. It it a trivia game in which five questions are asked to the player and the player is scored based on whether their responses are correct. It shows examples of text to speech, speech to text, and playing audio files.
+
+## Building & Running
+
+1. Recursively clone this repository with `git clone --recurse-submodules https://github.com/nkansal96/aurora-trivia-game.git`
+2. Run CMake to configure the project and install dependencies: `cmake .`
+3. Run the trivia game: `./bin/trivia_game`
---
+++ trivia.cpp
@@ -0,0 +1,89 @@
+#include <aurora/Aurora.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+
+struct TriviaQuestion {
+  std::string question;
+  std::vector<std::string> answers;
+};
+
+void setAuroraConfiguration() {
+  if(!getenv("APP_TOKEN")){
+    std::cerr << "ERROR: No \"APP_TOKEN\" enviornment variable set" << std::endl;
+    exit(1);
+  } else {
+    aurora::config.appToken = getenv("APP_TOKEN");
+  }
+  if(!getenv("APP_ID")) {
+    std::cerr << "ERROR: No \"APP_ID\" enviornment variable set"<< std::endl;
+    exit(1);
+  } else {
+    aurora::config.appID = getenv("APP_ID");
+  }
+
+  //device ID is optional, if not set, set as empty string
+  if(!getenv("DEVICE_ID")) {
+    aurora::config.deviceID = "";
+  } else {
+    aurora::config.deviceID = getenv("DEVICE_ID");
+  }
+}
+
+// returns true if answer is correct
+bool processAnswer(const std::vector<std::string> &possibleAnswers) {
+  aurora::ListenParams params;
+  params.length = 3.0;
+  params.silenceLen = 0.0;
+
+  aurora::Text answerText = aurora::listenAndTranscribe(params);
+
+  for (const std::string &answer : possibleAnswers) {
+    if (answerText == answer) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+int main(int argc, char **argv) {
+  setAuroraConfiguration();
+
+  aurora::Speech correctSpeech = aurora::Text("correct").speech();
+  aurora::Speech incorrectSpeech = aurora::Text("incorrect").speech();
+
+  int score = 0;
+
+  std::vector<TriviaQuestion> questions = {
+    {"what is the capital of california", {"Sacramento"}},
+    {"From which country did the Beatles originate", {"England", "United Kingdom", "Britain", "UK"}},
+    {"What song is Rebecca Black most famous for", {"Friday"}},
+    {"What country has paris as its capital", {"France"}},
+    {"Where is the empire state building located", {"New York"}}
+  };
+
+  for (TriviaQuestion &q : questions) {
+    aurora::Text(q.question).speech().getAudio().play();
+
+    if (processAnswer(q.answers)) {
+      score++;
+      correctSpeech.getAudio().play();
+    }
+    else {
+      incorrectSpeech.getAudio().play();
+    }
+  }
+
+  std::vector<std::string> scoreMessages = {
+    "You have a final score of zero. are you even trying?",
+    "You have a final score of one. At least you got the one",
+    "You have a final score of two. Almost half correct",
+    "You have a final score of three. A little over half correct",
+    "You have a final score of four. Almost perfect",
+    "You have a perfect score of five. congratulations"
+  };
+
+  aurora::Text(scoreMessages[score]).speech().getAudio().play();
+}
